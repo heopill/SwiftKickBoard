1233c213ff2caad5abc90a351a0d9e1fa8850bff
heopill <heopill@naver.com>
2025-04-25 15:18:14 +0900

    Initial Commit

---
+++ SwiftKickBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftKickBoard
+//
+//  Created by 허성필 on 4/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
