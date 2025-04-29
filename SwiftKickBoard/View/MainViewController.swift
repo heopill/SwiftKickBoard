ea813844f576b7237d4c5e06b7f49e99b930617b
HyoungYoon <mkl0285@naver.com>
2025-04-29 19:27:26 +0900

    feat: 메인뷰 지도UI추가 #15

--- SwiftKickBoard/View/MainViewController.swift
+++ SwiftKickBoard/View/MainViewController.swift
@@ -1,29 +1,14 @@
-//
-//  MainViewController.swift
-//  SwiftKickBoard
-//
-//  Created by 허성필 on 4/28/25.
-//
-
 import UIKit
+import NMapsMap
 
 class MainViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let mapView = NMFMapView(frame: view.bounds)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        view.addSubview(mapView)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
