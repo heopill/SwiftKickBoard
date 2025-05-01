225d300c4764cb23a13c922faaddfa012d9d6032
HyoungYoon <mkl0285@naver.com>
2025-05-02 01:16:57 +0900

    로그인 UI - 카카오 간편로그인 구현 #77

--- SwiftKickBoard/AppDelegate.swift
+++ SwiftKickBoard/AppDelegate.swift
@@ -11,6 +11,7 @@ import CoreData
 
 import NMapsMap
 
+import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        KakaoSDK.initSDK(appKey: "92aaaf307a669c25d12d765917adad84")
         // Override point for customization after application launch.
         return true
     }
